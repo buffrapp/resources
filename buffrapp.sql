70defca82307d633eab7d4d4c13c8429f59eb484
LeonelMoyano1809 <leonelmoyano1809@gmail.com>
2019-08-08 17:26:28 -0300

    base de datos con motivos

--- buffrapp.sql
+++ buffrapp.sql
@@ -1,26 +1,26 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
+-- version 4.0.4.2
+-- http://www.phpmyadmin.net
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 04-08-2019 a las 23:08:45
--- Versión del servidor: 10.1.31-MariaDB
--- Versión de PHP: 7.1.10
+-- Servidor: localhost
+-- Tiempo de generación: 08-08-2019 a las 20:25:21
+-- Versión del servidor: 5.6.13
+-- Versión de PHP: 5.4.17
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Base de datos: `buffrapp`
 --
+CREATE DATABASE IF NOT EXISTS `buffrapp` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `buffrapp`;
 
 -- --------------------------------------------------------
 
@@ -28,11 +28,12 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `administrador`
 --
 
-CREATE TABLE `administrador` (
+CREATE TABLE IF NOT EXISTS `administrador` (
   `Nombre` varchar(100) COLLATE utf8_spanish2_ci NOT NULL COMMENT 'NOMBRE DEL ADMINISTRADOR',
   `E-mail` varchar(100) COLLATE utf8_spanish2_ci DEFAULT NULL COMMENT 'E-MAIL DEL ADMINISTRADOR',
   `Password` varchar(16) COLLATE utf8_spanish2_ci DEFAULT NULL COMMENT 'CONTRASEÑA DEL ADMINISTRADOR PARA INICIAR SESION',
-  `DNI` int(9) NOT NULL COMMENT 'DNI DEL ADMINISTRADOR'
+  `DNI` int(9) NOT NULL COMMENT 'DNI DEL ADMINISTRADOR',
+  UNIQUE KEY `DNI` (`DNI`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci COMMENT='TABALA PARA LOS ADMINISTRADORES';
 
 --
@@ -48,11 +49,25 @@ INSERT INTO `administrador` (`Nombre`, `E-mail`, `Password`, `DNI`) VALUES
 -- Estructura de tabla para la tabla `horarios_de_atencion`
 --
 
-CREATE TABLE `horarios_de_atencion` (
+CREATE TABLE IF NOT EXISTS `horarios_de_atencion` (
   `Hora` time NOT NULL COMMENT 'HORA DE ATENCION',
   `Turno` varchar(7) COLLATE utf8_spanish2_ci NOT NULL COMMENT 'TURNO DE ATENCION (MAÑANA, NOCHE Y TARDE)',
-  `Dia` varchar(9) COLLATE utf8_spanish2_ci NOT NULL COMMENT 'DIA DE ATENCION (LUNES,MARTES,MIERCOLES,JUEVES Y VIERNES)'
+  `Dia` varchar(9) COLLATE utf8_spanish2_ci NOT NULL COMMENT 'DIA DE ATENCION (LUNES,MARTES,MIERCOLES,JUEVES Y VIERNES)',
+  PRIMARY KEY (`Turno`,`Dia`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci COMMENT='TABLA CON LOS HORARIOS DE ATENCION';
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `motivos`
+--
+
+CREATE TABLE IF NOT EXISTS `motivos` (
+  `ID_Motivo` int(3) NOT NULL AUTO_INCREMENT COMMENT 'ID que representa el motivo en las demas tablas',
+  `Motivo` varchar(100) COLLATE utf8_spanish_ci NOT NULL COMMENT 'Motivo',
+  `Tipo` tinyint(1) NOT NULL COMMENT 'Tipo de motivo (0=alumnos y 1=pedidos)',
+  PRIMARY KEY (`ID_Motivo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci COMMENT='TABLA CON LOS MOTIVOS' AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -60,17 +75,18 @@ CREATE TABLE `horarios_de_atencion` (
 -- Estructura de tabla para la tabla `pedidos`
 --
 
-CREATE TABLE `pedidos` (
+CREATE TABLE IF NOT EXISTS `pedidos` (
   `DNI_Usuario` int(9) NOT NULL COMMENT 'DNI DEL USUARIO QUE HIZO EL PEDIDO',
-  `ID_Pedido` int(11) NOT NULL COMMENT 'ID UNICA DEL PEDIDO',
+  `ID_Pedido` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID UNICA DEL PEDIDO',
   `ID_Producto` int(3) NOT NULL COMMENT 'ID DEL PRODUCTO PEDIDO',
-  `DNI_Administrador` int(9) NOT NULL COMMENT 'DNI del administrador que tomo el pedido',
+  `DNI_Administrador` int(9) DEFAULT NULL COMMENT 'DNI del administrador que tomo el pedido',
   `FH_Recibido` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `FH_Tomado` timestamp NULL DEFAULT NULL,
   `FH_Listo` timestamp NULL DEFAULT NULL,
   `FH_Entregado` timestamp NULL DEFAULT NULL,
-  `DNI_Cancelado` int(9) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci COMMENT='TABLA PARA LOS PEDIDOS';
+  `DNI_Cancelado` int(9) DEFAULT NULL,
+  PRIMARY KEY (`ID_Pedido`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci COMMENT='TABLA PARA LOS PEDIDOS' AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -78,12 +94,20 @@ CREATE TABLE `pedidos` (
 -- Estructura de tabla para la tabla `productos`
 --
 
-CREATE TABLE `productos` (
-  `ID_Producto` int(3) NOT NULL COMMENT 'ID UNICO DEL PRODUCTO',
+CREATE TABLE IF NOT EXISTS `productos` (
+  `ID_Producto` int(3) NOT NULL AUTO_INCREMENT COMMENT 'ID UNICO DEL PRODUCTO',
   `Nombre` varchar(100) COLLATE utf8_spanish2_ci NOT NULL COMMENT 'NOMBRE DEL PRODUCTO',
   `Precio` float NOT NULL COMMENT 'PRECIO DEL PRODUCTO',
-  `Estado` tinyint(1) NOT NULL COMMENT 'ESTADO DEL PRODUCTO'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci COMMENT='TABLA PARA LOS PRODUCTOS';
+  `Estado` tinyint(1) NOT NULL COMMENT 'ESTADO DEL PRODUCTO',
+  PRIMARY KEY (`ID_Producto`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci COMMENT='TABLA PARA LOS PRODUCTOS' AUTO_INCREMENT=4 ;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`ID_Producto`, `Nombre`, `Precio`, `Estado`) VALUES
+(1, 'asd', 123, 1);
 
 -- --------------------------------------------------------
 
@@ -91,11 +115,12 @@ CREATE TABLE `productos` (
 -- Estructura de tabla para la tabla `reportes`
 --
 
-CREATE TABLE `reportes` (
+CREATE TABLE IF NOT EXISTS `reportes` (
   `Motivo` text COLLATE utf8_spanish_ci NOT NULL COMMENT 'MOTIVO POR EL CUAL FUE REPORTADO',
   `DNI_U` int(9) NOT NULL COMMENT 'DNI DEL USUARIO',
   `DNI_A` int(9) NOT NULL COMMENT 'DNI DEL ADMINISTRADOR',
-  `Fecha_Hora` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'FECHA HORA DEL REPORTE'
+  `Fecha_Hora` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'FECHA HORA DEL REPORTE',
+  PRIMARY KEY (`DNI_U`,`Fecha_Hora`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
@@ -104,13 +129,14 @@ CREATE TABLE `reportes` (
 -- Estructura de tabla para la tabla `usuarios`
 --
 
-CREATE TABLE `usuarios` (
+CREATE TABLE IF NOT EXISTS `usuarios` (
   `DNI` int(9) NOT NULL COMMENT 'DNIDEL USUARIO REGISTRADO',
   `E-mail` varchar(100) COLLATE utf8_spanish2_ci DEFAULT NULL COMMENT 'E-MAIL DEL USUARIO REGISTRADO',
   `Password` varchar(16) COLLATE utf8_spanish2_ci DEFAULT NULL COMMENT 'CONTRASEÑA DEL ALUMNO REGISTRADO',
   `Nombre` varchar(100) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   `Curso` varchar(5) COLLATE utf8_spanish2_ci NOT NULL,
-  `Division` varchar(5) COLLATE utf8_spanish2_ci NOT NULL
+  `Division` varchar(5) COLLATE utf8_spanish2_ci NOT NULL,
+  PRIMARY KEY (`DNI`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci COMMENT='TABLA PARA LOS USUARIOS';
 
 --
@@ -119,63 +145,6 @@ CREATE TABLE `usuarios` (
 
 INSERT INTO `usuarios` (`DNI`, `E-mail`, `Password`, `Nombre`, `Curso`, `Division`) VALUES
 (42824297, NULL, NULL, 'Edgar Leonel Moyano', '7mo', '1ra');
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `administrador`
---
-ALTER TABLE `administrador`
-  ADD UNIQUE KEY `DNI` (`DNI`);
-
---
--- Indices de la tabla `horarios_de_atencion`
---
-ALTER TABLE `horarios_de_atencion`
-  ADD PRIMARY KEY (`Turno`,`Dia`);
-
---
--- Indices de la tabla `pedidos`
---
-ALTER TABLE `pedidos`
-  ADD PRIMARY KEY (`ID_Pedido`);
-
---
--- Indices de la tabla `productos`
---
-ALTER TABLE `productos`
-  ADD PRIMARY KEY (`ID_Producto`);
-
---
--- Indices de la tabla `reportes`
---
-ALTER TABLE `reportes`
-  ADD PRIMARY KEY (`DNI_U`,`Fecha_Hora`);
-
---
--- Indices de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`DNI`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `pedidos`
---
-ALTER TABLE `pedidos`
-  MODIFY `ID_Pedido` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID UNICA DEL PEDIDO';
-
---
--- AUTO_INCREMENT de la tabla `productos`
---
-ALTER TABLE `productos`
-  MODIFY `ID_Producto` int(3) NOT NULL AUTO_INCREMENT COMMENT 'ID UNICO DEL PRODUCTO';
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
